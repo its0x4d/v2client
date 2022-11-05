70fc99f83f21c69c6727a2e4ac6d83065984276d
Mostafa Mosavi <mostafa.uwsgi@gmail.com>
2022-11-05 12:51:59 +0330

    upgrade to version 1.0.0

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
 import setuptools
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open('README.md') as f:
+    long_description = f.read()
 
 setuptools.setup(
     name="v2client",
-    version="0.0.1",
+    version="1.0.0",
     author="Mostafa Mosavi",
     author_email="mostafa.uwsgi@gmail.com",
     description="A V2Ray/V2Fly client for Python",
-    long_description="A V2Ray/V2Fly client for Python",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/its0x4d/v2client",
     packages=setuptools.find_packages(),
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    license='MIT',
     python_requires='>=3.6',
 )
