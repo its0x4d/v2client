2cea555daba9a3d55aa34d731b7e7a447fb38214
Mostafa Mosavi <mostafa.uwsgi@gmail.com>
2022-11-06 09:42:42 +0330

    implement LogAPI

--- v2client/main.py
+++ v2client/main.py
@@ -1,6 +1,10 @@
+from v2client.v2ray.log import LogAPI
 from v2client.v2ray.proxy import ProxyAPI
 from v2client.v2ray.stats import StatsAPI
 
 
-class V2RayClient(StatsAPI, ProxyAPI):
+class V2RayClient(ProxyAPI, StatsAPI, LogAPI):
+    """
+    V2Ray Client API
+    """
     pass
